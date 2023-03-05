c1917ce900b86e649677aa7a12df46bcf12dd627
AleshkevichGleb <aleshkevichgleb05@gmail.com>
2023-03-05 15:18:14 +0300

    fix:tsak10

--- script.js
+++ script.js
@@ -2,81 +2,80 @@
 
 //task1
 {
-    // for(let i = 0; i <50; i++){
-    //     console.log(i)
-    // }
+    for(let i = 0; i <50; i++){
+        console.log(i)
+    }
 
-    // let a = 35;
-    // while(a>8){
-    //     console.log(a--)
-    // }
+    let a = 35;
+    while(a>8){
+        console.log(a--)
+    }
 }
 
 
 //task2
 {
-    // let a = 89; 
-    // while(a>11){
-    //     document.write(a-- + "<br/>")
-    // }
+    let a = 89; 
+    while(a>11){
+        document.write(a-- + "<br/>")
+    }
 }
 
 //task3
 {
-    // let sum = 0;
-    // for(let i = 0; i < 100; i++){
-    //     sum += i;
-    // }
-    // console.log(`Сумма чисел от 0 до 100: ${sum} `);
-    // console.log('\n')
+    let sum = 0;
+    for(let i = 0; i < 100; i++){
+        sum += i;
+    }
+    console.log(`Сумма чисел от 0 до 100: ${sum} `);
+    console.log('\n')
 }
 
 //task4
 {
-    // function getSum(a){
-    //     let sum = 0;
-    //     for(let i = 0; i < a; i++){
-    //         sum += i;
-    //         console.log(`${i}, Cумма чисел: ${sum}`)
-    //     }
-    //     console.log('\n')
-    // }
-    // getSum(5)
+    function getSum(a){
+        let sum = 0;
+        for(let i = 0; i < a; i++){
+            sum += i;
+            console.log(`${i}, Cумма чисел: ${sum}`)
+        }
+        console.log('\n')
+    }
+    getSum(5)
 }
 
 //task5
 {
-    // let i =8;
-    // for(; i < 56; i+=2){
-    //     console.log(i)
-    // }
-    // i = 8;
-    // while(i < 54){
-    //     console.log(i+=2)
-    // }
-    //console.log('\n')
+    let i =8;
+    for(; i < 56; i+=2){
+        console.log(i)
+    }
+    i = 8;
+    while(i < 54){
+        console.log(i+=2)
+    }
+    console.log('\n')
 }
 
 //task6
 {
-    // for(let i = 2; i < 10; i++){
-    //     for(let j = 2; j<10; j++){
-    //         console.log(`${i}*${j} = ${i*j}`)
-    //     }
-    //     console.log('\n')
-    // }
+    for(let i = 2; i < 10; i++){
+        for(let j = 2; j<10; j++){
+            console.log(`${i}*${j} = ${i*j}`)
+        }
+        console.log('\n')
+    }
 }
 
 //task7
 {
-    // let num = 0
-    // let n = 1000;
-    // for(; n > 50; n/=2){
-    //     console.log(n);
-    //     num++;
-    // }
-    // console.log(`Количество итераций: ${num}`)
-    // console.log('\n')
+    let num = 0
+    let n = 1000;
+    for(; n > 50; n/=2){
+        console.log(n);
+        num++;
+    }
+    console.log(`Количество итераций: ${num}`)
 }
 
 
@@ -98,6 +97,7 @@
     // console.log(`Сумма ${sum}`);
     // sum /= count;
     // console.log(`Среднее арифметическое ${sum}`)
+    // console.log('\n')
 }
 
 //task9
@@ -114,25 +114,23 @@
       }
     }
     console.log('Min:', min, 'Max:', max)
+    console.log('\n')
 }
 
 //task10
 {
-    // я вообще не понимаю как сделать это без массива
-    let obr = '';
+    let num = 0;
     let sum = 0;
     let count = 0;
-    let n = (Math.floor(Math.random()* 8000 + 1));
-    obr = String(n).split('').reverse().join("");
+    let n = String((Math.floor(Math.random()* 8000 + 1)));
     console.log('Число: ', n)
-    n = (String(n)).split('');
     for(let i = 0; i < n.length; i++){
-        sum += +n[i];
+        num = +n[i];
+        sum += +n[i]
         count++;
-        console.log(`${i+1}-е число: ${n[i]}`);
+        console.log(`${i+1}-е число: ${num}`);
     }
-    n = n.join("")
     console.log(`В ${n} ${count} числа`);
     console.log(`Сумма цифр числа ${n} = ${sum}`);
-    console.log(`${n} в обратном порядке: ${obr}`);
+    console.log(`${n} в обратном порядке: ${n.split('').reverse().join('')}`);
 }
